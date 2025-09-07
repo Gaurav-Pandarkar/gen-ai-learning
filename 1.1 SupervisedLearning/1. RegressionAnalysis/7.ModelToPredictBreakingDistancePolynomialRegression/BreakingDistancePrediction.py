66d2441f294415db34f1f9dc62603d2786b79067
Gaurav Pandarkar <gauravpandarkar5@dnyanyog.org>
2025-09-08 00:17:21 +0530

    feat:Implement save and load model code
        * Train the car distance prediction model and save it into file
        * Load the saved model file
        * Using loaded model predict the breaking distance

---
+++ 1.1 SupervisedLearning/1. RegressionAnalysis/7.ModelToPredictBreakingDistancePolynomialRegression/BreakingDistancePrediction.py
@@ -0,0 +1,39 @@
+import pandas as pd
+
+import matplotlib.pyplot as plt
+
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+
+
+df = pd.read_csv('data.csv')
+
+print(df)
+
+df.info()
+
+correlation = df['Speed'].corr(df['BrakingDistance'])
+
+print(correlation)
+
+plt.plot(df['Speed'], df['BrakingDistance'])
+plt.xlabel('Speed')
+plt.ylabel('BrakingDistance')
+plt.title('Speed vs BrakingDistance')
+plt.show()
+
+
+x = df.drop(columns=['BrakingDistance'],axis=1)
+y = df['BrakingDistance']
+
+poly = PolynomialFeatures(degree=5)  
+x_square = poly.fit_transform(x)
+
+print(x_square)
+
+model = LinearRegression()
+model.fit(x_square,y)
+
+output = model.predict(poly.fit_transform([[115]]))
+
+print("Degree 5 for 115",output)
---
+++ 1.1 SupervisedLearning/1. RegressionAnalysis/8.CarBreakPredictionModelSaveandLoad/LoadedModel.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from sklearn.preprocessing import PolynomialFeatures
+import pickle
+
+with open('car_prediction.bin','rb') as file:
+    model = pickle.load(file)
+
+poly = PolynomialFeatures(degree=5)
+data_to_predict = poly.fit_transform([[120]])
+
+print(data_to_predict)
+output = model.predict(data_to_predict)
+
+print("Breaking Distance",output)
